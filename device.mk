5eb87fd48f672f3244903b80c1bbb77e3bf6848d
Pranav Vashi <neobuddy89@gmail.com>
2025-01-22 20:21:43 +0530

    onyx: Do not install for clone apps or private space
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# DSP
+PRODUCT_PACKAGES += \
+    DSPVolumeSynchronizer
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
