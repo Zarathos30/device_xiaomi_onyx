f99d98c63d80d00b4a0c6ac59a876ee33f7590c0
Zarathos30 <vale.rigni@gmail.com>
2026-03-02 20:14:38 +0100

    onyx: update vendorsetup.sh
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,16 +3,21 @@
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
 rm -rf hardware/xiaomi
-git clone -b lineage-23.0 https://github.com/Zarathos30/android_hardware_xiaomi.git hardware/xiaomi
+git clone -b bka https://github.com/sm8635-dev/hardware_xiaomi.git hardware/xiaomi
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf device/xiaomi/onyx-kernel
-git clone -b axion https://github.com/Zarathos30/android_device_xiaomi_onyx-kernel-new.git  device/xiaomi/onyx-kernel
+git clone -b lineage-23.1 https://github.com/AxionAOSP-devices/android_kernel_xiaomi_onyx.git device/xiaomi/onyx-kernel
 
 # Google Camera (fresh clone)
 echo "Cloning Google Camera"
 rm -rf vendor/xiaomi/GoogleCamera
 git clone -b 16 https://github.com/Onyx-Hubs/vendor_xiaomi_GoogleCamera.git vendor/xiaomi/GoogleCamera
+
+# Lunaris Dolby (fresh clone)
+echo "Cloning Lunaris Dolby"
+rm -rf packages/apps/LunarisDolby
+git clone -b 16.0 https://github.com/unmoved21/packages_apps_LunarisDolby.git packages/apps/LunarisDolby
 
 echo "vendorsetup.sh execution complete."
